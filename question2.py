f0b83cd4ad7b6c87b458c5b4a97b3bddf5f1ff6b
Natasha Muthoni <natashaangela0553@gmail.com>
2026-02-20 13:15:24 +0300

    Remove extra files and keep only functions_task.py

--- question2.py
+++
@@ -1,7 +0,0 @@
-name = "Naima"
-age = 20
-is_AI_student = True
-
-print("Hello, my name is", name)
-print("I am",age,"years old")
-print("AI student status is",is_AI_student)
--- question4.py
+++
@@ -1,8 +0,0 @@
-# List of students
-students = ["Monicah", "Irwine", "Benedict", "Gloria", "Faith", "Teddy", "Sandra", "Drina", "Ryan"]
-
-# Loop through list
-index = 1
-for name in students:
-    print(index, name.upper())
-    index = index + 1
